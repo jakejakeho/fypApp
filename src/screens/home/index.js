f08139c43c6c85f8d0316c021a7d91bad9b04206
hojakejake <mtho@connect.ust.hk>
2019-01-22 15:53:15 +0800

    removed facebook token

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -13,71 +13,9 @@ var fbtoken = {
 };
 var tokenValid = false;
 class Home extends Component {
-  async storeToken(token) {
-    try {
-      //we want to wait for the Promise returned by AsyncStorage.setItem()
-      //to be resolved to the actual value before returning the value
-      var jsonOfItem = await AsyncStorage.setItem("fbtoken", JSON.stringify(token));
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  async retrieveTroken() {
-    try {
-      const retrievedItem = await AsyncStorage.getItem("fbtoken");
-      fbtoken = JSON.parse(retrievedItem);
-    } catch (error) {
-      console.log(error.message);
-    }
-    // calculate whether the token is expired
-    var milliseconds = (new Date).getTime() / 1000;
-    var difference = (fbtoken.expires - milliseconds);
-    tokenValid = !(difference < 0 || fbtoken === null || fbtoken.token === null || fbtoken.token === null);
-    if (tokenValid) {
-      this.navigateToMain();
-    }
-  }
-
-  async navigateToMain() {
-    const response = await fetch(`https://graph.facebook.com/me?access_token=${fbtoken.token}`);
-    Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
-    this.props.navigation.navigate("BasicFooter");
-  }
-  async logInFB() {
-    // check if there is valid fbtoken if not then login 
-    if (!tokenValid) {
-      try {
-        const {
-          type,
-          token,
-          expires,
-          permissions,
-          declinedPermissions,
-        } = await Expo.Facebook.logInWithReadPermissionsAsync(fbappID, {
-          permissions: ["public_profile"],
-        });
-        if (type === "success") {
-          // Get the user's name using Facebook's Graph API
-          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-          Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
-          // save token in asyncStoarge
-          this.props.storeToken({
-            "token": token,
-            "expires": expires,
-          });
-        } else {
-          // type === 'cancel'
-        }
-      } catch ({ message }) {
-        alert(`Facebook Login Error: ${message}`);
-      }
-    } else {
-      this.props.navigateToMain();
-    }
-  }
 
   render() {
-    this.retrieveTroken();
+    // this.retrieveTroken();
     return (
       <Container>
         <StatusBar barStyle="light-content" />
@@ -100,7 +38,10 @@ class Home extends Component {
           <View style={{ marginBottom: 80 }}>
             <Button
               style={{ backgroundColor: "#6FAF98", alignSelf: "center" }}
-              onPress={this.logInFB.bind(this)}
+              onPress={() => {
+                this.props.navigation.navigate("IconFooter");
+              }
+              }
             >
               <Text>Lets Go!</Text>
             </Button>
